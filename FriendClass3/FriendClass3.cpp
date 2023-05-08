eed2a23c77e6e33ceb27498566143191d9392316
fasyaaa <muhammad.a.f.2210@gmail.com>
2023-05-08 15:56:07 +0700

    15

---
+++ FriendClass3/FriendClass3.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class pelajar;
+class manusia {
+public:
+	void showNilaiPelajar(pelajar&);
+};
+
+class pelajar {
+private :
+	int nilai;
+public :
+	pelajar() {
+		nilai = 100;
+	}
+	friend void manusia::showNilaiPelajar(pelajar& x);
+};
+
+void manusia::showNilaiPelajar(pelajar& x) {
+	cout << "Nilai Pelajar : " << x.nilai;
+}
+
+int main() {
+
+}
