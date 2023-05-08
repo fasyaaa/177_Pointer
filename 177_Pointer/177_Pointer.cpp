662ede73d40f416a9da98acfdd8225233c4f4fee
fasyaaa <muhammad.a.f.2210@gmail.com>
2023-05-08 14:53:07 +0700

    3

--- 177_Pointer/177_Pointer.cpp
+++ 177_Pointer/177_Pointer.cpp
@@ -4,5 +4,15 @@ using namespace std;
 class mahasiswa {
 public :
 	int nim;
-	void showNim(); //Deklarasi methode
+	void showNim();									//Deklarasi methode
 };
+
+
+void mahasiswa::showNim() {							//Implememtasi methode diluar class
+	cout << "No Induk = " << nim << endl;
+}
+
+int main() {
+	mahasiswa mhs{ 1 };
+	mhs
+}
